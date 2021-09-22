95c65f08b6b742ee5ec95d30179704514a8d6a4d
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-09-23 00:20:26 +0200

    toegevoegd niet onder de 0

--- src/App.js
+++ src/App.js
@@ -22,7 +22,11 @@ function App() {
           <span className="emoji">🍓</span>
           <h2>Aardbeien</h2>
           <span className="fruitButtons">
-            <button onClick={() => setStrawberryValue(strawberryValue - 1)}>
+            <button onClick={() => {
+                if(strawberryValue === 0 ){
+                  console.log("can't have less");
+            }else{
+                setStrawberryValue(strawberryValue - 1)}}}>
               -
             </button>
             <span>{strawberryValue}</span>
@@ -35,7 +39,11 @@ function App() {
           <span className="emoji">🍌</span>
           <h2>Bananen</h2>
           <span className="fruitButtons">
-            <button onClick={() => setBananaValue(bananaValue - 1)}>
+            <button onClick={() => {
+              if(bananaValue === 0 ){
+                console.log("can't have less");
+              }else{
+                setBananaValue(bananaValue - 1)}}}>
               -
             </button>
             <span>{bananaValue}</span>
@@ -48,7 +56,11 @@ function App() {
           <span className="emoji">🍏</span>
           <h2>Appels</h2>
           <span className="fruitButtons">
-            <button onClick={() => setAppleValue(appleValue - 1)}>
+            <button onClick={() => {
+              if(appleValue === 0 ){
+                console.log("can't have less");
+              }else{
+                setAppleValue(appleValue - 1)}}}>
               -
             </button>
             <span>{appleValue}</span>
@@ -61,7 +73,11 @@ function App() {
           <span className="emoji">🥝</span>
           <h2>Kiwi's</h2>
           <span className="fruitButtons">
-            <button onClick={() => setKiwiValue(kiwiValue - 1)}>
+            <button onClick={() => {
+              if(kiwiValue === 0 ){
+                console.log("can't have less");
+              }else{
+                setKiwiValue(kiwiValue - 1)}}}>
               -
             </button>
             <span>{kiwiValue}</span>
