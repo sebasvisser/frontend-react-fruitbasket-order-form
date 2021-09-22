054901f46aaedd05f4b0a9a5f10a06110214810d
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-09-23 00:25:16 +0200

    formulier geimporteerd van reacthookform opdr.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [strawberryValue, setStrawberryValue] = React.useState(0);
-  const [bananaValue, setBananaValue] = React.useState(0);
-  const [appleValue, setAppleValue] = React.useState(0);
-  const [kiwiValue, setKiwiValue] = React.useState(0);
+  const [strawberryValue, setStrawberryValue] = useState(0);
+  const [bananaValue, setBananaValue] = useState(0);
+  const [appleValue, setAppleValue] = useState(0);
+  const [kiwiValue, setKiwiValue] = useState(0);
 
   function resetCounters() {
     setStrawberryValue(0);
@@ -15,8 +15,16 @@ function App() {
     console.log("The Great Reset")
   }
 
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({mode: "onBlur"});
+  const chosenFreq = watch('bezorgfrequentie');
+
+  function onSubmit(data){
+    console.log(data);
+  }
+
   return (
     <>
+      /*We beginnen met de fruit counter*/
       <h1>Fruitmand bezorgservice</h1>
         <div className="fruit" id="strawberry">
           <span className="emoji">🍓</span>
@@ -90,6 +98,105 @@ function App() {
           <button onClick={resetCounters}>RESET</button>
         </div>
 
+    /*  Start van het formulier (via React Hook Form) */
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Voornaam:
+          <input type="text" placeholder="Voornaam"
+                 {...register("voornaam",
+                     {required: "Voornaam mag niet leeg zijn"})}
+                 onBlur={handleSubmit}
+          />
+        </label>
+        {errors.voornaam && <p>{errors.voornaam.message}</p>}
+
+
+        <label>
+          Achternaam:
+          <input type="text" placeholder="Achternaam"
+                 {...register("achternaam",
+                     {required: "Achternaam mag niet leeg zijn"})}
+          />
+        </label>
+        {errors.achternaam && <p>{errors.achternaam.message}</p>}
+
+
+        <label>
+          Leeftijd:
+          <input type="number" placeholder="Leeftijd"
+                 {...register(
+                     "leeftijd",
+                     {required: true,
+                       min: {
+                         value: 18,
+                         message: "Minimaal 18 jaar oud.",
+                       }
+                     })}
+          />
+        </label>
+        {errors.leeftijd && <p>{errors.leeftijd.message}</p>}
+
+        <label>
+          Postcode:
+          <input type="text" placeholder="Postcode"
+                 {...register(
+                     "postcode",
+                     {required: "Postcode mag niet leeg zijn",
+                       minLength: {
+                         value: 6,
+                         message: "Postcode bestaat uit minimaal 6 tekens",
+                       },
+                       maxLength: {
+                         value: 6,
+                         message: "Postcode bestaat uit maximaal 6 tekens",
+                       },
+                       pattern: {
+                         value: /[0-9]{4}[a-z-A-Z]{2}/i ,
+                         message: "Postcode patroon: getal-getal-getal-getal-letter-letter",
+                       }
+                     })}
+          />
+        </label>
+        {errors.postcode && <p>{errors.postcode.message}</p>}
+
+        <label>
+          Bezorgfrequentie:
+          <select {...register("bezorgfrequentie", { required: true })}>
+            <option value="iedere week">Iedere week</option>
+            <option value="om de week">Om de week</option>
+            <option value="iedere maand">Iedere maand</option>
+            <option value="anders">Anders</option>
+          </select>
+        </label>
+        {errors.bezorgfrequentie && <p>{errors.bezorgfrequentie.message}</p>}
+
+        { chosenFreq === 'anders' && (
+            <label>
+              Voer een andere frequentie in:
+              <input type="text" placeholder="Andere frequentie"
+                     {...register("anders", {})} />
+            </label>
+        )}
+
+        <label>
+          Opmerkingen:
+          <textarea
+              {...register("opmerkingen", {})} />
+        </label>
+
+        <label>
+          Akkoord met de voorwaarden?
+          <input type="checkbox" placeholder="Akkoord?"
+                 {...register(
+                     "akkoord",
+                     {required: "Geen feestje zonder voorwaarden"}
+                 )}
+          />
+        </label>
+        {errors.akkoord && <p>{errors.akkoord.message}</p>}
+
+        <input type="submit" />
+      </form>
     </>
   );
 }
